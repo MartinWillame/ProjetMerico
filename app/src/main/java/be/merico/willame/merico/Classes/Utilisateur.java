a232afeb533e5ffdd4142a28fdc4311df3ef498e
Martin Willame <martin.willame@student.uclouvain.be>
2018-06-29 16:26:58 +0200

    Initial commit

---
+++ app/src/main/java/be/merico/willame/merico/Classes/Utilisateur.java
@@ -0,0 +1,9 @@
+package be.merico.willame.merico.Classes;
+
+/**
+ * Created by marti on 26-06-18.
+ */
+
+public class Utilisateur {
+
+}
---
+++ app/src/main/java/be/merico/willame/merico/Connection.java
@@ -0,0 +1,13 @@
+package be.merico.willame.merico;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Connection extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_connection);
+    }
+}

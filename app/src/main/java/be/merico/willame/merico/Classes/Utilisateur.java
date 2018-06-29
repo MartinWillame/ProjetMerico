bc0d5c1562fff45999a2a64761488e1fa94daabc
Unknown <martin.willame@student.uclouvain.be>
2018-06-29 16:31:02 +0200

    test commit

--- app/src/main/java/be/merico/willame/merico/Classes/Utilisateur.java
+++ app/src/main/java/be/merico/willame/merico/Classes/Utilisateur.java
@@ -6,4 +6,17 @@ package be.merico.willame.merico.Classes;
 
 public class Utilisateur {
 
+
+    private String motDePasse;
+    private String nom;
+    private String prenom;
+    private String identifiant;
+
+    public Utilisateur(int id, String motDePasse, String nom, String prenom, String identifiant, String photo, String mail, int bestFriend) {
+
+        this.motDePasse = motDePasse;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.identifiant = identifiant;
+    }
 }
